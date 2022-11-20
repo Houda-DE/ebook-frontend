cd8cc3257501e69be3ab9c367bcc0c3e1a9946db
Houda-DE <houdadebza8@gmail.com>
2022-11-20 18:18:13 +0100

    working on authentication

--- component/Authentication/index.jsx
+++ component/Authentication/index.jsx
@@ -1,18 +1,60 @@
 import { useRef, useState } from "react"
+import React from "react"
+import Form from '../Form'
+import styles from './styles.module.css'
 
 const authentication = () => {
 
-    const inputRef = useRef()
+    const usernameRef = useRef()
+    const emailRef = useRef()
+    const passRef = useRef()
+    const cpassRef = useRef()
     const [authState , setAuthState] = useState('signup')
 
+    const signupElements = [
+        {
+            id : 'name',
+            placeholder : 'User name',
+            ref : usernameRef
+        },
+        {
+            id : 'email',
+            placeholder : 'Email',
+            ref : emailRef
+        },
+        {
+            id : 'pass',
+            placeholder : 'Password',
+            ref : passRef
+        },
+        {
+            id : 'cpass',
+            placeholder : 'Confirm Password',
+            ref : cpassRef
+        }
+    ]
+
+    const SignupSubmit = (e) => {
+        e.preventDefault()
+        console.log({
+            username : usernameRef.current.value,
+            email : emailRef.current.value,
+            password : passRef.current.value,
+            passConfirm : cpassRef.current.value
+        })
+    }
+
     return(
-        <div>
+        <div className = {styles.container}>
+            <div>
+                <button>Sign up</button>
+                <button>Sign in</button>
+            </div>
             {
                 authState === 'signup' ? 
-                    ''
-                :   {
-
-                    }
+                   <Form title={'Sign up'} inputs={signupElements} submitButton={'Sign up'} options={[]} onSubmitFunc={SignupSubmit}/>
+                :   
+                    <div></div> 
             }
         </div>
     )
--- component/Form/index.jsx
+++ component/Form/index.jsx
@@ -1,6 +1,41 @@
-const form = ({placeholders , onChangeFunc , onSubmitFunc}) => {
+import styles from './styles.module.css'
+
+const form = ({title , inputs , options , submitButton , onSubmitFunc}) => {
     return(
-        <div></div>
+        <div className={styles.container}>
+            <div className={styles.title}>{title}</div>
+            <form onSubmit={onSubmitFunc} className={styles.form}>
+                <div className={styles.inputsContainer}>
+                    {
+                        inputs.map((element , index) => {
+                            return(
+                                <div className={styles.element}>
+                                    <div>{element.placeholder}:</div>
+                                    <input 
+                                        className={styles.input}
+                                        id = {element.id}
+                                        placeholder={`${element.placeholder}...`}
+                                        ref = {element.ref}
+                                    />
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+                <div>
+                    {
+                        options.map((element , index) =>{
+                            return(
+                                <div>
+                                    <button onClick = {(e) => element.onClickFunc(e)}>{element.name}</button>
+                                </div>
+                            )
+                        })
+                    }
+                </div>
+                <button className={styles.submitButton} type="submit">{submitButton}</button>
+            </form>
+        </div>
     )
 }
 export default form
